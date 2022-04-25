6ac3a836b2b605b079fe7b2e566d47a119c86536
plugin23 <xvalicek@stuba.sk>
2022-04-25 13:39:23 +0200

    lightning in second and third level adjusted, camera stabilizing added back

---
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Core/Base/Player/Camera/CameraStabilize.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStabilize : MonoBehaviour
+{
+    public float returnSpeed = 2.0f;
+    public Transform myTransform;
+
+    void LateUpdate()
+    {
+        myTransform.localRotation = Quaternion.Slerp(myTransform.localRotation, Quaternion.identity, Time.deltaTime * returnSpeed);
+    }
+}
